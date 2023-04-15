ef75aa8627e0af979e1ebdcbd3a5baf34a3d7067
Alvin Li <aflip24@gmail.com>
2023-04-15 04:15:12 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+function submitData(name, email) {
+    const requestInfo = {
+      name: name,
+      email: email
+    };
+  
+    const configurationObject = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(requestInfo)
+    };
+  
+    return fetch('http://localhost:3000/users', configurationObject)
+      .then(response => response.json())
+      .then(data => {
+        const id = data.id;
+        const newUser = document.createElement('div');
+        newUser.innerHTML = id;
+        document.body.appendChild(newUser);
+      })
+      .catch(error => {
+        const errorMessage = error.message;
+        const errorMsg = document.createElement('div');
+        errorMsg.innerHTML = errorMessage;
+        document.body.appendChild(errorMsg);
+      });
+  }
